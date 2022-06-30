1fa0a6c817daf91c4916ed36ba5f9bef80a881ac
James Grieve <jgrieve@ualberta.ca>
2022-06-30 10:02:32 -0600

    Arrays; Lists; Dictionaries; HashSets; Stacks and Queues;

--- Program.cs
+++ Program.cs
@@ -1,98 +1,78 @@
-﻿// Welcome to C#!
+﻿
+// ---
+// Arrays
+// ---
+// Arrays are very rigid. They have fixed physical sizes and require more mental math to work with in most cases.
 
-// In order to write something to the console, we use Console.WriteLine().
-// This is very similar to output() in the web console, or console.log() in the debug console.
-Console.WriteLine("Hello, World!");
+int[] myIntegerArray = new int[10];
+int myIntergerArrayLogical = 0;
 
-// In order to read something from the console, we use Console.ReadLine().
-// Typically, the ReadLine() would be assigned to a variable to store the result, but it can also be used for "Press enter to continue..." prompts.
-Console.WriteLine("Press enter to continue...");
+// Sets the next item in the arrays and updates logical size.
+myIntegerArray[myIntergerArrayLogical++] = 10;
+myIntegerArray[myIntergerArrayLogical++] = 15;
+myIntegerArray[myIntergerArrayLogical++] = 25;
+
+// Removes the last item in the array and updates logical size.
+myIntegerArray[--myIntergerArrayLogical] = 0;
+
+
+string[,] myTwoDArray = new string[5, 5];
+
+
+// ---
+// Lists
+// ---
+// Lists are more similar to JS arrays, with add/remove/etc methods to manage the math for you.
+
+List<string> myList = new List<string>();
+
+myList.Add("Hello");
+
+// ---
+// Dictionaries
+// ---
+// Dictionaries are like lists, but can be indexed by things other than integers.
+
+Dictionary<string, string> myDictionary = new Dictionary<string, string>();
+
+myDictionary.Add("first", "Hello, World!");
+
+Console.WriteLine(myDictionary["first"]);
+
+// ---
+// HashSets
+// ---
+// HashSets are like lists, but everything in them must be unique. They function much faster than lists do in most cases.
+
+HashSet<double> myHashset = new HashSet<double>();
+
+myHashset.Add(3.14);
+myHashset.Add(1.21);
+myHashset.Add(10.12);
+myHashset.Add(1.21); // Adds nothing, as it already exists.
+
+// ---
+// Queues and Stacks
+// ---
+// Queues function on a first in, first out basis (like a lineup at a grocery store).
+// Stacks function on a first in, last out basis (like a stack of paper).
+
+Queue<int> myQueue = new Queue<int>();
+Stack<int> myStack = new Stack<int>();
+
+myQueue.Enqueue(1);
+myQueue.Enqueue(2);
+myQueue.Enqueue(3);
+
+myStack.Push(1);
+myStack.Push(2);
+myStack.Push(3);
+
+Console.WriteLine(myQueue.Dequeue()); // 1
+Console.WriteLine(myStack.Pop()); // 3
+
+
+
+
+
 Console.ReadLine();
-
-// A lot of the syntax for C# is idenitcal to JavaScript. This includes comments, delimiters (semicolons for statements and braces for code blocks), and general operators.
-
-// Two main differences are that C# is strongly typed while JavaScript is weakly typed, and C# is compiled while JavaScript is interpreted.
-
-int myInt = 0; // NOT let or var.
-
-// Compiled means that C# converts the entire program to machine language ahead of time, while JavaScript reads each line one by one.
-// Typically this will mean that syntax errors, etc will be caught before the program even runs, meaning unhandled exceptions will be less common in that scenario.
-float myFloat = 0.5f;
-string userName;
-
-Console.Write("Please enter your name: ");
-userName = Console.ReadLine();
-Console.WriteLine("Hello, " + userName + "!");
-
-Console.Write("Please enter a number between 1 and 10: ");
-
-// Since converting a user input to an integer is "dangerous", we put it in a try so that we can catch any exceptions.
-try
-{
-    myInt = int.Parse(Console.ReadLine());
-    // This method also works to convert.
-    Convert.ToInt32("1");
-    if (myInt < 1 || myInt > 10)
-    {
-        Console.WriteLine("You're not great at these instructions...");
-    }
-    else
-    {
-        for (int i = 1; i <= myInt; i++)
-        {
-            Console.Write(i + " ");
-        }
-        Console.WriteLine();
-    }
-}
-catch (Exception e)
-{
-    Console.WriteLine("ERROR: " + e.Message);
-}
-
-// Since validation takes space, we can move the validation into a dedicated function to keep our main program concise. 
-GetIntSafe("Please enter any valid number: ");
-
-// Ternary operators are essentially an if/else in one line (condition ? valIfTrue : valIfFalse):
-Console.WriteLine("Your number is: " + (myInt % 2 == 1 ? "Odd" : "Even"));
-
-
-// Docstrings allow other developers to see what a function does at a glance.
-
-int GetIntSafe(string prompt)
-// Safely returns an int input from the user, based on the prompt. No formatting is done to the prompt, it is expected to be ready to go.
-{
-    int output = 0;
-    bool valid = false;
-    while (!valid)
-    {
-        Console.Write(prompt);
-        try
-        {
-            output = int.Parse(Console.ReadLine());
-            valid = true;
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("ERROR: " + e.Message);
-            Console.WriteLine("Please try again.");
-        }
-    }
-    return output;
-}
-
-
-
-int var1 = 10, var2 = 5, var3=0;
-Console.WriteLine("Var1: " + var1 + " Var2: " + var2 + " Var3: " + var3);
-ExampleFunction(var1, ref var2, out var3);
-Console.WriteLine("Var1: " + var1 + " Var2: " + var2 + " Var3: " + var3);
-
-// Normal parameters can't modify their arguments, reference parameters can.
-// Output parameters set the argument value after the function ends (like a second return).
-void ExampleFunction(int number, ref int reference, out int output)
-{
-    number++;
-    reference++;
-    output = 1;
-}
